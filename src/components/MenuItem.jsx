a8a325bd225fe211437cf68a7c543f4383832316
BasselAlShaar <basselalshaar11@gmail.com>
2025-05-15 19:55:35 +0300

    fixed sizes of image

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,28 +2,44 @@ import React from 'react';
 import '../styles/MenuItem.css';
 
 const MenuItem = ({ item, openModal }) => {
-  return (
-    <div className="menu-item" onClick={() => openModal(
-      item.englishTitle,
-      item.arabicTitle,
-      item.price,
-      item.englishDescription,
-      item.arabicDescription,
-      item.image
-    )}>
-      <img src={item.image} alt={item.englishTitle} className="menu-item-img" />
-      <div className="menu-item-content">
-        <div className="menu-item-text">
-          <h3 className="english-text">{item.englishTitle}<span className="price">{item.price}</span></h3>
-          {item.englishDescription && <p className="english-text">{item.englishDescription}</p>}
-        </div>
-        <div className="arabic-text">
-          <div className="arabic-title arabic-text">{item.arabicTitle}</div>
-          {item.arabicDescription && <p className="arabic-text">{item.arabicDescription}</p>}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className='menu-item'
+			onClick={() =>
+				openModal(
+					item.englishTitle,
+					item.arabicTitle,
+					item.price,
+					item.englishDescription,
+					item.arabicDescription,
+					item.image
+				)
+			}
+		>
+			<img
+				src={item.image}
+				alt={item.englishTitle}
+				className='menu-item-img'
+			/>
+			<div className='menu-item-content'>
+				<div className='menu-item-text'>
+					<div className='price-container'>
+						<h3 className='english-title'>{item.englishTitle}</h3>
+						<h3 className='price'>{item.price}</h3>
+					</div>
+					{item.englishDescription && (
+						<p className='english-text'>{item.englishDescription}</p>
+					)}
+				</div>
+				<div className='arabic-text'>
+					<div className='arabic-title'>{item.arabicTitle}</div>
+					{item.arabicDescription && (
+						<p className='arabic-text'>{item.arabicDescription}</p>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MenuItem;
