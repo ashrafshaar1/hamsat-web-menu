fa4c90c5245535cf89b7627146f9ef35e3f54527
BasselAlShaar <basselalshaar11@gmail.com>
2025-05-15 15:50:51 +0300

    fixed menu section css

--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import MenuItem from './MenuItem';
-import '../styles/MenuItem.css';
+import '../styles/MenuSection.css';
 
 const MenuSection = ({ section, title, description, items, openModal }) => {
-  return (
-    <section id={section} className="menu-section">
-      <h2>{title}</h2>
-      <div className="section-description">
-        <p className="english-text">{description.english}</p>
-        <p className="arabic-text">{description.arabic}</p>
-      </div>
-      
-      {items.map(item => (
-        <MenuItem key={item.id} item={item} openModal={openModal} />
-      ))}
-    </section>
-  );
+	return (
+		<section
+			id={section}
+			className='menu-section'
+		>
+			<h2>{title}</h2>
+			<div className='section-description'>
+				<p className='english-text'>{description.english}</p>
+				<p className='arabic-text'>{description.arabic}</p>
+			</div>
+
+			{items.map((item) => (
+				<MenuItem
+					key={item.id}
+					item={item}
+					openModal={openModal}
+				/>
+			))}
+		</section>
+	);
 };
 
 export default MenuSection;
