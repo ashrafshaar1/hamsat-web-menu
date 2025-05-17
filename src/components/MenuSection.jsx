ae65e5170c780789e1840305c5c62f61eca4ebae
ashrafshaar1 <ashrafshaar540@gmail.com>
2025-05-17 18:51:47 +0300

    bug fixes

--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -3,26 +3,37 @@ import MenuItem from './MenuItem';
 import '../styles/MenuSection.css';
 
 const MenuSection = ({ section, title, description, items, openModal }) => {
-	return (
-		<section
-			id={section}
-			className='menu-section'
-		>
-			<h2>{title}</h2>
-			<div className='section-description'>
-				<p className='english-text'>{description.english}</p>
-				<p className='arabic-text'>{description.arabic}</p>
-			</div>
+  const squareLayoutSections = [
+    'freshjuice', 
+    'milkshake', 
+    'hamsatcocktails', 
+    'coffeedrinks', 
+    'softdrinks', 
+    'shisha'
+  ];
 
-			{items.map((item) => (
-				<MenuItem
-					key={item.id}
-					item={item}
-					openModal={openModal}
-				/>
-			))}
-		</section>
-	);
+  const isSquareLayout = squareLayoutSections.includes(section);
+
+  return (
+    <section
+      id={section}
+      className={`menu-section ${isSquareLayout ? 'square-layout' : ''}`}
+    >
+      <h2>{title}</h2>
+      <div className="section-description">
+        <p className="english-text">{description.english}</p>
+        <p className="arabic-text">{description.arabic}</p>
+      </div>
+
+      {items.map((item) => (
+        <MenuItem
+          key={item.id}
+          item={item}
+          openModal={openModal}
+        />
+      ))}
+    </section>
+  );
 };
 
 export default MenuSection;
