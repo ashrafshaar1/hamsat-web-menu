1c390feddcc2406ba2f577bb05ad5f564e9e2848
CyberwareGames <cyberwaregames11@gmail.com>
2025-05-17 20:15:55 +0300

    fixed arabic title place

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../styles/MenuItem.css';
+import React from "react";
+import "../styles/MenuItem.css";
 
 const MenuItem = ({ item, openModal }) => {
   return (
@@ -16,25 +16,21 @@ const MenuItem = ({ item, openModal }) => {
         )
       }
     >
-      <img
-        src={item.image}
-        alt={item.englishTitle}
-        className="menu-item-img"
-      />
+      <img src={item.image} alt={item.englishTitle} className="menu-item-img" />
       <div className="menu-item-content">
         <div className="menu-item-text">
           <div className="price-container">
             <h3 className="english-title">{item.englishTitle}</h3>
-            <div className="arabic-title">{item.arabicTitle}</div>
             <div className="price">{item.price}</div>
           </div>
           {item.englishDescription && (
             <p className="english-text">{item.englishDescription}</p>
           )}
+          <div className="arabic-title">{item.arabicTitle}</div>
+          {item.arabicDescription && (
+            <p className="arabic-text">{item.arabicDescription}</p>
+          )}
         </div>
-        {item.arabicDescription && (
-          <p className="arabic-text">{item.arabicDescription}</p>
-        )}
       </div>
     </div>
   );
